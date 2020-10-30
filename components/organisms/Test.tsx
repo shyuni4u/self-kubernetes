dc4c6cfe64e00661e3b3fe5c20d7bbeedc90cfa1
sunwoo <shyuniz@naver.com>
2020-10-30 07:25:04 +0000

    test API

--- components/organisms/Test.tsx
+++ components/organisms/Test.tsx
@@ -1,33 +1,24 @@
-import React, { useEffect } from 'react';
-import k8s from '@kubernetes/client-node';
-
-// import Http from '../atoms/KubeAPI';
+import React, { useEffect, useState } from 'react';
+import KubeHttp from '../atoms/KubeAPI';
 
 export const Todo: React.FC = () => {
+  const [namespaces, setNamespaces] = useState<any>(null)
   useEffect(() => {
-    const kc = new k8s.KubeConfig();
-    kc.loadFromDefault();
-
-    const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-
-    k8sApi.listNamespacedPod('default')
-      .then((res) => {
-        console.log(res.body);
+    const onLoadTest = async () => {
+      await KubeHttp.get('/api/v1/namespaces', {
+        // params: { query: text }
+      }).then(response => {
+        setNamespaces(response)
+      }).catch(error => {
+        console.error(error)
       });
+    }
+    onLoadTest();
   }, [])
-  // useEffect(() => {
-  //   const onLoadTest = async () => {
-  //     const res = await Http.get('/api/v1/namespaces', {
-  //       // params: { query: text }
-  //     });
-  //     console.log(res)
-  //   }
-  //   onLoadTest();
-  // }, [])
 
   return (
     <>
-      test
+      {namespaces.data.items.map(item => item.metadata.name)}
     </>
   );
 };
