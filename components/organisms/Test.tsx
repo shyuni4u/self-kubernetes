e0e1e4474ec50b2198b07e7637911c2044d1b32f
sunwoo <shyuniz@naver.com>
2020-10-30 04:49:16 +0000

    update

--- components/organisms/Test.tsx
+++ components/organisms/Test.tsx
@@ -1,16 +1,29 @@
 import React, { useEffect } from 'react';
-import Http from '../atoms/KubeAPI';
+import k8s from '@kubernetes/client-node';
+
+// import Http from '../atoms/KubeAPI';
 
 export const Todo: React.FC = () => {
   useEffect(() => {
-    const onLoadTest = async () => {
-      const res = await Http.get('/api/v1/namespaces', {
-        // params: { query: text }
+    const kc = new k8s.KubeConfig();
+    kc.loadFromDefault();
+
+    const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+
+    k8sApi.listNamespacedPod('default')
+      .then((res) => {
+        console.log(res.body);
       });
-      console.log(res)
-    }
-    onLoadTest();
   }, [])
+  // useEffect(() => {
+  //   const onLoadTest = async () => {
+  //     const res = await Http.get('/api/v1/namespaces', {
+  //       // params: { query: text }
+  //     });
+  //     console.log(res)
+  //   }
+  //   onLoadTest();
+  // }, [])
 
   return (
     <>
