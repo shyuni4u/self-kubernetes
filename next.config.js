f6a39a795dcd88a675261eea93b2ce6fbe909938
shyuni4u <shyuniz@naver.com>
2020-11-17 01:03:47 +0000

    update config for static website

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   trailingSlash: true,
-  exportPathMap: function() {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
     return {
-      '/': { page: '/' }
+      '/': { page: '/' },
+      '/amd/dashboard1': { page: '/amd/dashboard1' },
+      '/amd/dashboard2': { page: '/amd/dashboard2' },
+      '/nvidia/dashboard': { page: '/nvidia/dashboard' },
+      '/total/manual': { page: '/total/manual' },
+      '/total/dashboard': { page: '/total/dashboard' }
     };
   }
 }
